90de6af58fe09267213ec4a6ae12a379c901fb47
mandykqh <kaw.mandy@gmail.com>
2024-03-29 11:11:25 +0800

    add peerprep and bitebalance

--- app.js
+++ app.js
@@ -26,18 +26,51 @@ const projects = [
         }
     },
     {
-        id: 'bloom1',
-        title: 'Bloom',
-        overview: 'Our team of 5 were involved in this project. We came up with {linkToAffinityDiagram}, and created prototypes.',
-        role: 'Product Designer' + '\n' + 'User Research, Visual Design, Prototyping',
+        id: 'bitebalance',
+        title: 'BiteBalance',
+        overview: 'BiteBalance is a collaborative UI/UX design project undertaken by a team of five individuals, aimed at enhancing the meal planning experience through personalization. Our team meticulously followed each phase of the design process, iterating through Contextual Analysis, Data Analysis utilizing affinity diagramming techniques, crafting personas, developing scenarios and storyboards, prototyping, and conducting Usability Testing via heuristic evaluations.',
+        role: 'Product Designer, Researcher \n• Involved in all phases of the design process',
         links: {
-            linkToAffinityDiagram: '<a href="https://www.glorialo.design/" target="_blank" rel="noopener noreferrer">our affinity diagram</a>'
+
         },
         images: [
-            'assets/img/Bloom/catalogue-main.png',
-            'assets/img/Bloom/catalogue-view.png',
-        ]
-    }
+            'assets/img/BiteBalance/1.png',
+            'assets/img/BiteBalance/2.png',
+            'assets/img/BiteBalance/3.png',
+            'assets/img/BiteBalance/4.png',
+            'assets/img/BiteBalance/5.png',
+            'assets/img/BiteBalance/6.png',
+            'assets/img/BiteBalance/7.png',
+            'assets/img/BiteBalance/8.png',
+        ],
+        date: 'Aug - Nov 2023',
+        technologies: 'Figma, Balsamiq',
+        buttons: {
+            "See Website": "https://niveytha-waran.my.canva.site/",
+            "See Final Prototype": "https://www.figma.com/file/8OCjkIEQSbXsGHsdC9H3m1/BiteBalance-G3-Final-Prototype?type=design&node-id=3%3A1251&mode=design&t=FIJPR1tWCNMbdagt-1"
+        }
+    },
+    {
+        id: 'peerprep',
+        title: 'PeerPrep',
+        overview: 'PeerPrep is a fullstack web application led by a team of 5, designed to enhance the pair-programming experience by matching users together to collaborate on LeetCode-style problems.\n\n Features include:\n• Sign up or log in to your account\n• Browse, filter, and search the question repository by title or difficulty\n• Match with another peer based on question preferences\n• Collaborate in real-time with your matched peer through coding, drawing, or chatting\n• Seamlessly switch between questions during collaboration sessions\n• Track past question attempts and view a dashboard of your performance',
+        role: 'Frontend Developer \n • Prototyping UI designs on Figma\n• Developing the frontend UI of the application',
+        links: {
+            linkToUserJourneys: '<a href="https://www.figma.com/proto/Q1LcfnQuR7W8HeMVMSzKaO/CS3240-Individual-Assignment?type=design&node-id=14-942&t=Bv010b8DzfrsnrXo-1&scaling=scale-down&page-id=0%3A1&mode=design" target="_blank" rel="noopener noreferrer">user journeys</a>',
+            linkToRapidPrototypes: '<a href="https://www.figma.com/proto/Q1LcfnQuR7W8HeMVMSzKaO/CS3240-Individual-Assignment?type=design&node-id=301-4120&t=8DDzgkG8hFBclaRC-0&scaling=scale-down&page-id=301%3A4118" target="_blank" rel="noopener noreferrer">rapid prototypes</a>',
+            linkToLofiPrototypes: '<a href="https://www.figma.com/proto/Q1LcfnQuR7W8HeMVMSzKaO/CS3240-Individual-Assignment?type=design&node-id=357-3&t=Bc6M7HCxO6hm5ZFD-1&scaling=scale-down&page-id=357%3A2&mode=design" target="_blank" rel="noopener noreferrer">lo-fi prototypes</a>',
+            // linkToHifiPrototypes: '<a href="https://www.figma.com/proto/Q1LcfnQuR7W8HeMVMSzKaO/CS3240-Individual-Assignment?type=design&node-id=154-2709&t=5yGtvlmU4wwApuOt-0&scaling=scale-down&page-id=154%3A2337&starting-point-node-id=154%3A2709" target="_blank" rel="noopener noreferrer">hi-fi prototypes</a>',
+        },
+        images: [
+            'assets/img/PeerPrep/main.png',
+        ],
+        date: 'Aug - Nov 2023',
+        technologies: 'TypeScript, ReactJS, Chakra UI, Figma, MongoDB, PostgreSQL, ExpressJS, NodeJS, AWS Elastic Beanstalk, Docker',
+        buttons: {
+            "See Report": "https://docs.google.com/document/d/118UUeZfu11_gqHaTm0ltfEe8dfjk5yT2Jg9ytyX42wk/edit?usp=sharing",
+            "Go to GitHub": "https://github.com/CS3219-AY2324S1/ay2324s1-course-assessment-g15",
+        }
+    },
 
 ];
 function loadProject(projectId) {
@@ -57,6 +90,7 @@ function loadProject(projectId) {
         document.getElementById('project-role-text').innerHTML = project.role.replace(/\n/g, '<br>');
 
         const prototypeContainer = document.querySelector('.project-prototype');
+        prototypeContainer.classList.add(projectId);
         prototypeContainer.innerHTML = '';
 
         project.images.forEach(imagePath => {
@@ -67,18 +101,18 @@ function loadProject(projectId) {
             prototypeContainer.appendChild(img);
         });
         const resourcesContainer = document.querySelector('.project-buttons');
-        resourcesContainer.innerHTML = ''; // Clear existing content
+        resourcesContainer.innerHTML = '';
 
-        // Check if there are resources to display
+
         if (Object.keys(project.buttons).length > 0) {
             for (const buttonName in project.buttons) {
                 if (project.buttons.hasOwnProperty(buttonName)) {
                     const buttonUrl = project.buttons[buttonName];
                     const button = document.createElement('button');
-                    button.textContent = buttonName; // Button text from the resource key
-                    button.className = 'btn btn-primary'; // Apply Bootstrap button styles
-                    button.onclick = function () { window.open(buttonUrl, '_blank'); }; // Open the link in a new tab when clicked
-                    resourcesContainer.appendChild(button); // Append the button to the container
+                    button.textContent = buttonName;
+                    button.className = 'btn btn-primary';
+                    button.onclick = function () { window.open(buttonUrl, '_blank'); };
+                    resourcesContainer.appendChild(button);
                 }
             }
         } else {
